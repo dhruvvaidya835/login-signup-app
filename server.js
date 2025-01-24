5b21ad0ba21dc2708c24948c208b671555d19279
Dhruv83 <dhruvvaidya835@gmail.com>
2025-01-24 15:49:17 +0530

    login

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+// Import the User model
+const User = require('./models/user');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://Test1234:Test1234@cluster0.wjoqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB:', err));
+
+// Routes
+// Render login page
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+// Render signup page
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// Handle signup form submission
+app.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        // Check if the username already exists
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.send('Username already exists. Please choose another.');
+        }
+
+        // Save the new user to the database
+        const newUser = new User({ username, password });
+        await newUser.save();
+        res.redirect('/');
+    } catch (error) {
+        res.status(500).send('An error occurred during signup.');
+    }
+});
+
+// Handle login form submission
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        // Check if the username and password match
+        const user = await User.findOne({ username, password });
+        if (user) {
+            res.render('home', { username });
+        } else {
+            res.send('Invalid username or password.');
+        }
+    } catch (error) {
+        res.status(500).send('An error occurred during login.');
+    }
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
